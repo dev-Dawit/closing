04fa26b66c4e1c48ec1053f6995a46a0a0527e8e
Dawit-dev <dawitgetachew3811@gmail.com>
2022-10-18 04:22:26 +0300

    completed sign-up form

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,81 @@
+import {useState} from 'react';
+
+import {createAuthUserWithEmailAndPassword, createUserDocumetFromAuth} from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+
+/* steps to setup sign-in form
+    step 1- create functional component 'signinForm' and return a div with the form
+    step 2- prepare onChange tag on each input field to track when the input in a field changes
+    step 3- create a an object to hold all states as a group 'defaultFormFields' and initialize it with empty strings
+    step 4- use 'useState' react function to store the input typed in the fields and initialize it with 'defaultFormFields'
+    step 5- create a function 'handleChange' which sets the input fields with the value the user types 
+    step 6- create 'name' and 'value' tags to distigush the fields and know which field is firing onChange event   
+    step 7- distructure the 'name' and 'value' from the event of onChange method
+    step 8- spread other inputs that did not trigger an event(the field the user is not typing on) and only set the field that triggerd the event to store that value in the state 'formFields' 
+    setp 9- make the value of the a particular field 'value' tag, value of that particular field from the state 'formFields'. this will make the value of that input equal to the value of the state
+*/
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '' 
+}
+
+const SigninForm = () => {
+
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {displayName, email, password, confirmPassword} = formFields; 
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if(password !== confirmPassword) {
+            alert("passwords do not much");    
+            return;
+        };
+        
+        //creating/storing the user in firestore
+        try{
+            const {user} = await createAuthUserWithEmailAndPassword(email, password)
+            console.log(user);
+            await createUserDocumetFromAuth(user, {displayName});
+            resetFormFields();
+
+        }
+        catch(error){
+            if(error.code == 'auth/email-already-in-use'){
+                alert('cannot create user, email already in use')
+            }
+            console.log('user creation encountered an error', error)
+           
+        }
+
+    }
+
+    //console.log(formFields)
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        setFormFields({...formFields, [name]: value})      //spreads-in all 'formFields' object properties(key-value pairs) with their state values, and set the value of the input that triggerd the event(the name that matches with the 'name' tag). 
+    } 
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <FormInput label= 'Display Name' inputOptions = {{type:'text', required: true, onChange:handleChange, name:'displayName', value:displayName}} />
+                
+                <FormInput label= 'Email' inputOptions = {{type:'text', required: true, onChange:handleChange, name:'email', value:email}}/>
+                
+                <FormInput label= 'Password' inputOptions = {{type:'text', required: true, onChange:handleChange, name:'password', value:password}}/>
+                
+                <FormInput label= 'Confirm Password' inputOptions = {{type:'text', required: true, onChange:handleChange, name:'confirmPassword', value:confirmPassword}}/>
+
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SigninForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,22 @@
-import {signInWithGooglePopup} from '../../utils/firebase/firebase.utils';
+import {signInWithGooglePopup, createUserDocumetFromAuth} from '../../utils/firebase/firebase.utils';
+
+import SigninForm from '../../components/sign-in-form/sign-in-form.component'
 
 const SignIn = () => {
+
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const {user} = await signInWithGooglePopup();  //Poping up sign in with Google on our sign-in page.then destructuring 'user' property from the response obj we got from signInWithPopup b/c that is the only property we care abt
+        console.log(user);
+        const userDocRef = await createUserDocumetFromAuth(user);  //storing the Authenticated user in firestore db 
     };
 
     return(
         <div>
             <h2>This is the sign-in page</h2>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={logGoogleUser}>
+                Sign in with Google Popup
+            </button>
+            <SigninForm />
         </div>
     )
  }
