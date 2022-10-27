96b13baed9cca071a8ccff2e96d0359c9b48b8e1
Dawit-dev <dawitgetachew3811@gmail.com>
2022-10-27 22:56:18 +0300

    created and styled the cart-icon and cart-dropdown without any functionalities

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import {UserContext} from '../../contexts/user.context';
 import {signOutUser} from '../../utils/firebase/firebase.utils.js';
 import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
@@ -29,8 +32,9 @@ const Navigation = () => {
                         </Link>
                     )
                 }
-                
+                <CartIcon />
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
